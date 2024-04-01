539449e3469c5ba9874a0bb0cfedf50896e0656e
Ivan Tetyukhin <itetyukhin@digital-spirit.ru>
2024-04-01 14:19:33 +0400

    lab1/cread class list

--- src/main/java/org/example/List.java
+++ src/main/java/org/example/List.java
@@ -24,7 +24,72 @@ public class List {
     }
   }
 
+  public boolean delElem(Integer index){
+    if(index>=size){
+      return false;
+    }
+    else{
+      if(index == 0){
+        head = head.next;
+      }
+      else{
+        Node ptr = head;
 
+        for(int i = 0; i < index-1; i++){
+          ptr = ptr.next;
+        }
+
+        ptr.next = ptr.next.next;
+      }
+      --size;
+      return true;
+    }
+  }
+
+  public boolean editElem(Integer elem, Integer index){
+    if(index>=size){
+      return false;
+    }
+    else{
+      if(index == 0){
+        head = head.next;
+      }
+      else{
+        Node ptr = head;
+        for(int i = 0; i < index; i++){
+          ptr = ptr.next;
+        }
+        ptr.data = elem;
+      }
+      return true;
+    }
+  }
+
+  public int getElem(int index){
+    if(index >= size){
+      throw new ArrayIndexOutOfBoundsException();
+    }
+    else{
+      Node ptr = head;
+      for(int i = 0; i < index; i++){
+        ptr = ptr.next;
+      }
+      return ptr.data;
+    }
+  }
+
+  @Override
+  public String toString(){
+    String listString = "";
+    Node ptr = head;
+
+    for(int i=0;i<size;++i){
+      listString +=ptr.data + " ";
+      ptr = ptr.next;
+    }
+
+    return listString;
+  }
   private class Node{
     private Integer data;
     private Node next;
